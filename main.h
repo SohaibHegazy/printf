c4a6190ee8a5ecea326cfdac19a87e2cf367b2d9
shereenmhussein <shereenmhussein@gmail.com>
2023-11-13 07:25:10 -0800

    modifying main.h and creating functions.c

--- main.h
+++ main.h
@@ -1,9 +1,22 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
 #include <unistd.h>
 #include <string.h>
+#include <stdarg.h>
+
+
+
+typedef struct specifier
+{
+        char *c;
+        int (*f)(char *, va_list);
+} spec;
 
 int _printf(const char *format, ...);
+int (*get_func(const char *s))(char *, va_list);
+int func_c(char *, va_list);
+int func_s(char *, va_list);
+int func_perc(char *, va_list);
 
 
 #endif
