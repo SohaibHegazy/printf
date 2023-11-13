800164e74bd0c55bca2dd0a1013cfc6833915780
“SohaibHegazy” <sohaib.mohamed@hotmail.comgit config --global user.email sohaib.mohamed@hotmail.com>
2023-11-12 23:39:25 -0800

    write function.c file and get_functions file

---
+++ get_func.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * get_func - function to select the operation
+ *
+ * @s: the specifier
+ *
+ * Return: function
+ *
+ */
+
+int (*get_func(const char *s))(char *)
+{
+	spec sp[] = {
+		{"c", func_c},
+		{"s", func_s},
+		{"%", func_perc},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 3)
+	{
+		if (s && sp[i].c[0] == s[0])
+			return (sp[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *
+ * _printf - like printf
+ *
+ * @format: input string
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0, n = 0, count = 0;
+	int (*string)(char *);
+
+	va_list args;
+
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++, n++)
+	{
+		if (format[i] != '%')
+			write(1, &format[i], sizeof(char));
+		else
+		{
+			i++;
+			count = 0;
+			string = get_func(&format[i]);
+			count = string(va_arg(args, char *));
+			n = n + count - 1;
+		}
+	}
+	return (n);
+	va_end(args);
+}
+
