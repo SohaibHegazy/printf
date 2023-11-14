8674018724e9847a79e498e341269b2115812bc7
shereenmhussein <shereenmhussein@gmail.com>
2023-11-14 07:58:29 -0800

    modify functions.c

--- functions.c
+++ functions.c
@@ -19,3 +19,45 @@ int print_s(va_list args, int n)
 	n += len;
 	return (n);
 }
+
+/**
+ * print_int - like printf
+ *
+ * @args: list of arguments
+ * @n: number of characters printed
+ *
+ * Return: n
+ */
+
+int print_int(va_list args, int n)
+{
+	char num[50];
+	int d = va_arg(args, int);
+	int len;
+
+	sprintf(num, "%d", d);
+
+	for (len = 0; num[len] != '\0'; len++)
+	;
+	write(1, &num, len);
+	n += len;
+	return (n);
+}
+
+/**
+ * print_c - like printf
+ *
+ * @args: list of arguments
+ * @n: number of characters printed
+ *
+ * Return: n
+ */
+
+int print_c(va_list args, int n)
+{
+	char c = va_arg(args, int);
+
+	write(1, &c, 1);
+	n++;
+	return (n);
+}
