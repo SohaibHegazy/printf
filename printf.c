793d671f0d8026dc719c487cf766076ca51cb8eb
shereenmhussein <shereenmhussein@gmail.com>
2023-11-14 09:01:07 -0800

    modify typo

--- printf.c
+++ printf.c
@@ -11,14 +11,14 @@
 
 int print_s(va_list args, int n)
 {
-        char *string = va_arg(args, char *);
-        int len;
+	char *string = va_arg(args, char *);
+	int len;
 
-        for (len = 0; string[len] != '\0'; len++)
-                ;
-        write(1, &string, len);
-        n += len;
-        return (n);
+	for (len = 0; string[len] != '\0'; len++)
+		;
+	write(1, &string, len);
+	n += len;
+	return (n);
 }
 
 
