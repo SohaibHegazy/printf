12486f996cac364d47ad69b7545f6f66a5d27c66
“SohaibHegazy” <sohaib.mohamed@hotmail.comgit config --global user.email sohaib.mohamed@hotmail.com>
2023-11-14 05:17:23 -0800

    modify typo

--- main.h
+++ main.h
@@ -8,17 +8,8 @@
 #include <stdio.h>
 
 
-typedef struct specifier
-{
-        char *c;
-        int (*f)(va_list, param);
-} spec;
 
 int _printf(const char *format, ...);
-int (*get_func(char *s))(va_list, param);
-int func_c(char *, va_list);
-int func_s(char *, va_list);
-int func_perc(char *, va_list);
 
 
 #endif
